cd5291a5da1162419a66a6214a9c8c1cf1da040c
aymen2782 <aymen.sellaouti@gmail.com>
2022-04-15 12:24:49 +0100

    router + twig

--- src/Controller/SecondController.php
+++ src/Controller/SecondController.php
@@ -7,12 +7,16 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+#[Route('second')]
 class SecondController extends AbstractController
 {
-    #[Route('/second/{name}', name: 'app_second')]
+    #[Route('/{name<[01]?\d{1,2}>}', name: 'app_second')]
     public function index($name, Request $request): Response
     {
-        dump($request);
+//        dump($request);
+//        if($name % 3 == 0) {
+//            $this->addFlash('success','ca marche :)');
+//        }
         return $this->render('second/index.html.twig', [
             'controller_name' => 'SecondController',
             'esm' => $name,
